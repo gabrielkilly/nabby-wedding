4f0ed30c7a57136aea1a160df203aa21952e5c27
Gabriel Killy <gabykilly@gmail.com>
2023-03-22 17:43:23 -0500

    first draft itin page

--- pages/itinerary.tsx
+++ pages/itinerary.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Navbar, { NavItems } from '@/components/Navbar'
 import Head from 'next/head'
 
-const sections: ItinerarySectionProps[] = [
+const itnSections: ItinerarySectionProps[] = [
   {
     enTitle: "ICEBREAKER",
     esTitle: "ROMPEHELIOS",
@@ -74,7 +74,7 @@ interface ItinerarySectionProps {
 
 const ItinerarySection = ({enTitle, esTitle, date, meetingPoints, color, iconPath}: ItinerarySectionProps) => {
     return (
-      <div className={`flex flex-col ${color} justify-center items-center mt-8 py-8 w-[95%]`}>
+      <div className={`flex flex-col ${color} justify-center items-center mt-8 py-8 w-full lg:w-[95%]`}>
         <h5 className='text-gray-800'><img className="inline mr-3" src={iconPath}/>{enTitle} <span className='italic'>/ {esTitle}</span></h5>
         <h4 className='small mt-8 text-gray-800'>{date}</h4>
         <>{meetingPoints.map(point => MeetingPoint(point))}</>
@@ -87,10 +87,40 @@ const MeetingPoint = (point: IteneraryMeetingPoint) => {
             return (
               <div className='flex flex-col justify-center items-center my-3'>
                 <p className='p2 mt-2'><span className='text-gray-800'>{timeRange}</span> <span className='mx-3'>|</span><span className='text-gray-600'>{location}</span></p>
-                <p className='p3 mt-2'>{address} <a href={mapUrl} target="_blank" className='ml-2 link'>View Map</a></p>
+                <div className='flex flex-col lg:flex-row justify-center items-center'>
+                  <p className='p3 mt-2'>{address}</p>
+                  <a href={mapUrl} target="_blank" className='ml-2 link'>View Map</a>
+                </div>
               </div>
             )
 }
+
+const dressCodeSections: DressCodeSectionProps[] = [
+  {
+    title: "ICEBREAKER",
+    label: "Casual/Semi-formal",
+    description: "Casual summer evening attire. "
+  },
+  {
+    title: "CEREMONY & RECEPTION",
+    label: "Formal",
+    description: "Long dresses and suits. "
+  }
+]
+interface DressCodeSectionProps {
+  title: string, 
+  label: string, 
+  description: string
+}
+
+const DressCodeSection = ({title, label, description}: DressCodeSectionProps) =>
+  <div className='flex flex-col justify-center items-center my-4'>
+    <h5>{title}</h5>
+    <div className='flex flex-col lg:flex-row justify-center items-center my-4'>
+      <p><strong>{label}</strong></p>
+      <p>{description}</p>
+    </div>
+  </div>
 
 export default function Itinerary() {
   return (
@@ -103,10 +133,15 @@ export default function Itinerary() {
       </Head>
       <main>
           <Navbar tabname={NavItems.itinerary.name} />
-          <div className="flex flex-col">
-            <div className='flex flex-col justify-center items-center p-8'>
+          <div className="flex flex-col items-center">
+            <div className='flex flex-col justify-center items-center lg:p-8 w-full'>
                 <h2>Itinerary</h2>
-                {sections.map(sections =>  ItinerarySection(sections))}
+                {itnSections.map(sections =>  ItinerarySection(sections))}
+                <hr className='w-[95%] mt-8 border-t-2 border-solid border-[#D3D3D3]'/>
+            </div>
+            <div className='flex flex-col justify-center items-center px-4 pt-4 pb-16'>
+              <h2 className='mb-3'>Dress Code</h2>
+              {dressCodeSections.map(section => DressCodeSection(section))}
             </div>
           </div>
       </main>
